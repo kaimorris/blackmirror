4983772e7ec24858bd023a594942e4afcad93a6f
kaimorris <kaimorrislove7@gmail.com>
2017-12-11 19:49:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,42 +3,28 @@
 function playGlass(e){
 	document.getElementById("glass-shatter").play();
 }
-function toggleMovieModeFmf(elem){
+function toggleMovieMode(elem){
 	document.getElementById("opacity1").classList.toggle("opacity");
 	document.getElementById("opacity2").classList.toggle("opacity");
 	document.getElementById("opacity3").classList.toggle("opacity");
-}
-function toggleMovieModeEntire(elem){
 	document.getElementById("opacity4").classList.toggle("opacity");
 	document.getElementById("opacity5").classList.toggle("opacity");
 	document.getElementById("opacity6").classList.toggle("opacity");
-}
-function toggleMovieModeBrb(elem){
 	document.getElementById("opacity7").classList.toggle("opacity");
 	document.getElementById("opacity8").classList.toggle("opacity");
 	document.getElementById("opacity9").classList.toggle("opacity");
-}
-function toggleMovieModeWhiteBear(elem){
 	document.getElementById("opacity10").classList.toggle("opacity");
 	document.getElementById("opacity11").classList.toggle("opacity");
 	document.getElementById("opacity12").classList.toggle("opacity");
-}
-function toggleMovieModeNosedive(elem){
 	document.getElementById("opacity13").classList.toggle("opacity");
 	document.getElementById("opacity14").classList.toggle("opacity");
 	document.getElementById("opacity15").classList.toggle("opacity");
-}
-function toggleMovieModePlaytest(elem){
 	document.getElementById("opacity16").classList.toggle("opacity");
 	document.getElementById("opacity17").classList.toggle("opacity");
 	document.getElementById("opacity18").classList.toggle("opacity");
-}
-function toggleMovieModeSanjun(elem){
 	document.getElementById("opacity19").classList.toggle("opacity");
 	document.getElementById("opacity20").classList.toggle("opacity");
 	document.getElementById("opacity21").classList.toggle("opacity");
-}
-function toggleMovieModeHated(elem){
 	document.getElementById("opacity22").classList.toggle("opacity");
 	document.getElementById("opacity23").classList.toggle("opacity");
 	document.getElementById("opacity24").classList.toggle("opacity");
