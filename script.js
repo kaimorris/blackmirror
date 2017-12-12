b312b5485f310cb0fed973a3eaed1b72f18ef113
kaimorris <kaimorrislove7@gmail.com>
2017-12-11 19:18:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,25 @@ function toggleMovieMode(elem){
 	document.getElementById("opacity1").classList.toggle("opacity");
 	document.getElementById("opacity2").classList.toggle("opacity");
 	document.getElementById("opacity3").classList.toggle("opacity");
+	document.getElementById("opacity4").classList.toggle("opacity");
+	document.getElementById("opacity5").classList.toggle("opacity");
+	document.getElementById("opacity6").classList.toggle("opacity");
+	document.getElementById("opacity7").classList.toggle("opacity");
+	document.getElementById("opacity8").classList.toggle("opacity");
+	document.getElementById("opacity9").classList.toggle("opacity");
+	document.getElementById("opacity10").classList.toggle("opacity");
+	document.getElementById("opacity11").classList.toggle("opacity");
+	document.getElementById("opacity12").classList.toggle("opacity");
+	document.getElementById("opacity13").classList.toggle("opacity");
+	document.getElementById("opacity14").classList.toggle("opacity");
+	document.getElementById("opacity15").classList.toggle("opacity");
+	document.getElementById("opacity16").classList.toggle("opacity");
+	document.getElementById("opacity17").classList.toggle("opacity");
+	document.getElementById("opacity18").classList.toggle("opacity");
+	document.getElementById("opacity19").classList.toggle("opacity");
+	document.getElementById("opacity20").classList.toggle("opacity");
+	document.getElementById("opacity21").classList.toggle("opacity");
+	document.getElementById("opacity22").classList.toggle("opacity");
+	document.getElementById("opacity23").classList.toggle("opacity");
+	document.getElementById("opacity24").classList.toggle("opacity");
 }
